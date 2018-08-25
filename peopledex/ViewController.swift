c8786e8fa98a9a2e03f7558971eaf3922903acc4
Elisa <elisawd08@gmail.com>
2018-08-25 13:00:24 -0400

    Initial Commit

---
+++ peopledex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  peopledex
+//
+//  Created by Owner on 2018-08-25.
+//  Copyright © 2018 hackthe6ix team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
